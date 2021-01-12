742580f5ee4bf83c143e8809138423f05940804c
Eliud Juárez <juarez_s3@hotmail.com>
2021-01-11 21:41:40 -0600

    Se agregó routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,23 +3,36 @@ import { Routes, RouterModule } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { HomeComponent } from './components/home/home.component';
+import { LayoutComponent } from './components/layout/layout.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductsComponent } from './components/products/products.component';
 
 const routes: Routes = [
   {
-    path: '', redirectTo:'/home', pathMatch: 'full'
-  },
-  {
-    path: 'home', component: HomeComponent
-  },
-  {
-    path: 'products', component: ProductsComponent
-  },
-  {
-    path: 'contact', component: ContactComponent
-  },
+    path: '',
+    component: LayoutComponent,    
+    children: [
+      {
+        path: '',
+        redirectTo:'/home', 
+        pathMatch: 'full'
+      },
+      {
+        path: 'home', component: HomeComponent
+      },
+      {
+        path: 'products', component: ProductsComponent
+      },
+      {
+        path: 'products/:id', component: ProductDetailComponent
+      },
+      {
+        path: 'contact', component: ContactComponent
+      }
+    ]
+  },  
   {
     path: 'demo', component: DemoComponent
   },
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -7,54 +8,14 @@ import { Product } from 'src/app/models/product';
 })
 export class ProductsComponent implements OnInit {
 
-   products: Product[] = [
-    {
-      id: '1',
-      image: 'assets/images/camiseta.png',
-      title: 'Camiseta',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '2',
-      image: 'assets/images/hoodie.png',
-      title: 'Hoodie',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '3',
-      image: 'assets/images/mug.png',
-      title: 'Mug',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '4',
-      image: 'assets/images/pin.png',
-      title: 'Pin',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '5',
-      image: 'assets/images/stickers1.png',
-      title: 'Stickers',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '6',
-      image: 'assets/images/stickers2.png',
-      title: 'Stickers',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-  ];
+  products: Product[];
 
-  constructor() { }
+  constructor(
+    private productsService: ProductsService
+  ) { }
 
   ngOnInit(): void {
+    this.products = this.productsService.getAllProducts();
   }
 
   clickProduct(id: number) {
