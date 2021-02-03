35108f07492156078dbc88c36397624215745439
Eliud <juarez_s3@hotmail.com>
2021-02-03 01:31:04 -0600

    HttpClient

--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -1,64 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Product } from '../../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductsService {
+export class ProductsService {  
 
-  products: Product[] = [
-    {
-      id: '1',
-      image: 'assets/images/camiseta.png',
-      title: 'Camiseta',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '2',
-      image: 'assets/images/hoodie.png',
-      title: 'Hoodie',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '3',
-      image: 'assets/images/mug.png',
-      title: 'Mug',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '4',
-      image: 'assets/images/pin.png',
-      title: 'Pin',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '5',
-      image: 'assets/images/stickers1.png',
-      title: 'Stickers',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '6',
-      image: 'assets/images/stickers2.png',
-      title: 'Stickers',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-  ];
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
-
-  getAllProducts() {
-    return this.products;
+  getAllProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${environment.url_api}/products`);
   }
 
-  getProduct(id: string) {
-    return this.products.find(item => id === item.id);
+  getProduct(id: string): Observable<Product> {
+    return this.http.get<Product>(`${environment.url_api}/products/${id}`);
   }
 
 }
