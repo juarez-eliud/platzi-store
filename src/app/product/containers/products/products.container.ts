963c0142362d4674c621486e4e79dd23074b5437
Eliud Juárez <juarez_s3@hotmail.com>
2021-09-22 22:49:37 -0500

    Short imports

--- src/app/product/containers/products/products.container.ts
+++ src/app/product/containers/products/products.container.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
-import { ProductsService } from 'src/app/core/services/products.service';
+import { ProductsService } from '@core/services/products.service';
 
 @Component({
   selector: 'app-products',
