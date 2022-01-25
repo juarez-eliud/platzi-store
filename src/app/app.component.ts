cf7754bfa9b45094409aa25094ea30f5915ab765
Eliud Juárez <juarez_s3@hotmail.com>
2022-01-25 00:39:25 -0600

    Google Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Product } from './models/product';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,21 @@ import { Product } from './models/product';
 })
 export class AppComponent {
 
+  //Implementación de Google Analytics
+  constructor( private router: Router) {
+    const navEndEvents$ = this.router.events
+    .pipe(
+      //Se filtran los eventos cuando se cambia de una página a otra
+      filter(event => event instanceof NavigationEnd)
+    );
+    /* Cada vez que haya un cambio de una página a otro se envía la 
+    información a Google Analytics, escucha un flujo de datos cada vez que
+    se termina la navegación*/
+    navEndEvents$.subscribe((event: NavigationEnd) => {
+      gtag('config', 'UA-147051588-1', {
+        //Se envía que ruta es la que está cambiando
+        page_path: event.urlAfterRedirects
+      });
+    });
+  }
 }
